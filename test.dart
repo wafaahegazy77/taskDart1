3e54d273f33d27964956f6587027cd725a243cbe
wafaahegazy77 <wafaahegazy7720@gmail.com>
2021-07-05 18:52:10 +0200

    Last

--- test.dart
+++ test.dart
@@ -7,20 +7,10 @@ void main(){
   stdout.write('Enter first number: ');
   var num1 = stdin.readLineSync();
   var firstNum = int.parse(num1 ?? '0');
-  // if (firstNum == null) {
-  //   print("bad number");
-  // }else {
-  //   print('First number is $firstNum' );
-  // }
 
   stdout.write('Enter second number: ');
   var num2 = stdin.readLineSync();
   var secondNum = int.parse(num2 ?? '0');
-  if (secondNum == null) {
-    print("bad number");
-  }else {
-    print('Second number is $secondNum' );
-  }
 
   stdout.write('Choose operation ( + , - , * , /): ');
   var operation = stdin.readLineSync();
