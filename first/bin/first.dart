6c78b77e6d40b6a94422a860cb200783cf3202d2
wafaahegazy77 <wafaahegazy7720@gmail.com>
2021-07-05 21:29:01 +0200

    finaaal

--- first/bin/first.dart
+++ first/bin/first.dart
@@ -1,3 +1,35 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+
+  // First task - Calculator of 2 numbers -> operations + - * /
+
+  stdout.write('Enter first number: ');
+  var num1 = stdin.readLineSync();
+  var firstNum = int.parse(num1 ?? '0');
+
+  stdout.write('Enter second number: ');
+  var num2 = stdin.readLineSync();
+  var secondNum = int.parse(num2 ?? '0');
+
+  stdout.write('Choose operation ( + , - , * , /): ');
+  var operation = stdin.readLineSync();
+  print('Operation is $operation');
+
+  switch(operation){
+    case '+':
+      print('First number + second number = ${firstNum + secondNum}');
+      break;
+    case '-':
+      print('First number - second number = ${firstNum - secondNum}');
+      break;
+    case '*':
+      print('First number * second number = ${firstNum * secondNum}');
+      break;
+    case '/':
+      print('First number / second number = ${firstNum / secondNum}');
+      break;
+
+  }
+
 }
