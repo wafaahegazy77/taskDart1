122475a3c709cc91307a0baf577ce3d448da2c9f
wafaahegazy77 <wafaahegazy7720@gmail.com>
2021-07-05 17:18:59 +0200

    task finished

--- test.dart
+++ test.dart
@@ -6,16 +6,16 @@ void main(){
 
   stdout.write('Enter first number: ');
   var num1 = stdin.readLineSync();
-  var firstNum = int.tryParse(num1 ?? '');
-  if (firstNum == null) {
-    print("bad number");
-  }else {
-    print('First number is $firstNum' );
-  }
+  var firstNum = int.parse(num1 ?? '0');
+  // if (firstNum == null) {
+  //   print("bad number");
+  // }else {
+  //   print('First number is $firstNum' );
+  // }
 
   stdout.write('Enter second number: ');
   var num2 = stdin.readLineSync();
-  var secondNum = int.tryParse(num2 ?? '');
+  var secondNum = int.parse(num2 ?? '0');
   if (secondNum == null) {
     print("bad number");
   }else {
@@ -27,13 +27,13 @@ void main(){
   print('Operation is $operation');
 
   if(operation == '+'){
-    print('First number + second number = $firstNum + $secondNum');
+    print('First number + second number = ${firstNum + secondNum}');
     // print(firstNum + secondNum);
   } else if(operation == '-'){
-    print('First number - second number = $firstNum - $secondNum');
+    print('First number - second number = ${firstNum - secondNum}');
   } else if(operation == '*'){
-    print('First number * second number = $firstNum * $secondNum');
+    print('First number * second number = ${firstNum * secondNum}');
   } else  if(operation == '/'){
-    print('First number / second number = $firstNum/$secondNum ');
+    print('First number / second number = ${firstNum / secondNum}');
   }
 }
